85b5cd6f8d0cdef8646b31deea74fd4e145a3456
Artur Czajka <arturczajka@gmail.com>
2016-05-09 13:26:02 +0200

    Unification of MVar example

--- app/Main04.hs
+++ app/Main04.hs
@@ -2,17 +2,20 @@
 
 import           Blaze.ByteString.Builder           (fromByteString)
 import           Blaze.ByteString.Builder.Char.Utf8 (fromShow)
-import           Control.Concurrent.MVar            (modifyMVar, newMVar)
+import           Control.Concurrent.MVar            (MVar, modifyMVar, newMVar)
 import           Data.Monoid                        ((<>))
 import           Network.HTTP.Types                 (status200)
-import           Network.Wai                        (responseBuilder)
+import           Network.Wai                        (Application,
+                                                     responseBuilder)
 import           Network.Wai.Handler.Warp           (run)
 
+main :: IO ()
 main = do
     visitorCount <- newMVar 0
-    run 8080 $ application visitorCount
+    run 8080 $ app visitorCount
 
-application countRef _ sendResponse =
+app :: MVar Int -> Application
+app countRef _ sendResponse =
     modifyMVar countRef $ \count -> do
         let count' = count + 1
             -- łączenie ze sobą builderów oszczędza pamięć, mniej obiektów tymczasowych
